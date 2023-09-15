b005f02b435b8a835e059956c45cc087149dcd68
v-chenx83 <1614819989@qq.com>
2023-09-15 20:14:30 +0800

    “第二次提交”

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <h1>SVG测试代码</h1>
-    <!-- svg:图标外层容器节点，内部需要与use标签结合使用 -->
-    <svg-icon
-      name="refresh"
-      color="red"
-      width="160px"
-      height="160px"
-    ></svg-icon>
+    <h1>测试axios二次封装</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import SvgIcon from '@/components/SvgIcon/index.vue'
+import request from '@/utills/request'
+import { onMounted } from 'vue'
+
+//当组件挂载完毕发送请求
+onMounted(() => {
+  console.log('1111')
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+})
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,16 @@ const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn, //国际化配置
 })
-
-console.log(import.meta.env)
 //svg插件需要的配置代码
 import 'virtual:svg-icons-register'
+
+//引入自定义插件对象
+import gloalComponent from '@/components';
+//安装自定义插件
+app.use(gloalComponent);
+
+//引入模板的全局样式
+import '@/styles/index.scss'
 
 //将应用挂载到挂载点上
 app.mount('#app')
